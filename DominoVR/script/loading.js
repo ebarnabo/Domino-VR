851489e0b4cc6786e0f14c1af818488900b134eb
ebarnabo <edwinbarnabot@gmail.com>
2024-02-12 17:29:30 -0400

    Derniers Changement 12 Fev 2024

--- DominoVR/script/loading.js
+++ DominoVR/script/loading.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
                 });
             });
-        }
+        }///
         showGameUI();
     });
 });
