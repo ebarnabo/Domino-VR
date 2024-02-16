0c9cb911c854a3118543bc0c4b45f2f6297b44c5
ebarnabo <edwinbarnabot@gmail.com>
2024-02-15 22:48:44 -0400

    14

--- DominoVR/script/loading.js
+++ DominoVR/script/loading.js
@@ -50,18 +50,6 @@ document.addEventListener('DOMContentLoaded', function() {
         pauseButton.classList.remove('hide');
         updateDominoTextures();
         updateModel();
-                    // turn controller's physics presence on only while button held down
-                    AFRAME.registerComponent('phase-shift', {
-                        init: function () {
-                          var el = this.el
-                          el.addEventListener('gripdown', function () {
-                            el.setAttribute('collision-filter', {collisionForces: true})
-                          })
-                          el.addEventListener('gripup', function () {
-                            el.setAttribute('collision-filter', {collisionForces: false})
-                          })
-                        }
-                      })
         // Le bouton Pause est déjà inclus dans 'menuingame', donc il sera affiché avec son conteneur
     }
 
